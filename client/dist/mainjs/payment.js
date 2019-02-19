97ea8ce7b63d87282345a90fdf00a20c3b3c6097
hyuk0824 <kh_0824@naver.com>
2019-02-19 22:43:39 +0900

    index, payment

---
+++ client/dist/mainjs/payment.js
@@ -0,0 +1,18 @@
+$('#trans').click(function(){
+    
+    var ceo_code = 0; 
+
+    $.ajax({
+        url: "/payment_server_side.js",
+        type: "POST",
+        datatype: 'json',
+        data: {
+             zipcode: ceo_code
+         }
+    }).done(function(result){
+
+        $('account').val = result.account;
+        $('money').val = result.money;
+    })
+    }
+)
